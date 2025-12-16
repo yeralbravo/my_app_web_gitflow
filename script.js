1b1b4f860986b41bea6b7e6b574f72232d5280fc
Yeral <yeraldinbravo65@gmail.com>
2025-12-16 11:46:17 -0500

    Añadir formulario de contacto

--- script.js
+++ script.js
@@ -2,3 +2,10 @@ document.getElementById('btnClick').addEventListener('click', function() {
     document.getElementById('output').textContent = '¡Botón clicado!';
 });
 
+//Este es un metodo
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    document.getElementById('formOutput').textContent = 'Nombre: ' + name + ', Email: ' + email;
+});
